a59cdf0db4893ad856a38422c30dec30dc570cb2
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-11-30 23:43:10 +0000

    Modified main.py

--- bin/main.py
+++ bin/main.py
@@ -9,4 +9,15 @@ app = App()
 
 # Main loop
 while app.returnStatement != "QUIT":
-    pass  # TODO: Stub
+    # Checking returnStatement and running
+    #  the corresponding screen
+    if app.returnStatement == "-MAIN-":
+        pass  # TODO: Stub
+    if app.returnStatement == "LAUNCH-MODS":
+        pass  # TODO: Stub
+    if app.returnStatement == "MANAGE":
+        pass  # TODO: Stub
+    if app.returnStatement == "SETTINGS":
+        pass  # TODO: Stub
+    if app.returnStatement == "DOWNLOAD":
+        pass  # TODO: Stub
