a244bbb84d3902f7559cc98f4dc4c7dec640fd2d
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-11-30 22:23:13 +0000

    Created lib/main.py

---
+++ bin/main.py
@@ -0,0 +1,13 @@
+# TotkModLauncher/bin/main.py
+# Contains main program
+
+# Importing Modules and Packages
+from lib.app import App
+
+# Creating App
+app = App()
+
+
+# Main loop
+while app.returnStatement != "QUIT":
+    pass  # TODO: Stub
