9fed7737c6e074d4635ee3a3b43197b9e5c5bb17
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-12-01 23:48:05 +0000

    Added main_menu.py

---
+++ gui/main_menu.py
@@ -0,0 +1,11 @@
+# TotkModLauncher/gui/main_menu.py
+# Contains main_menu function
+
+# Importing dependencies
+import tkinter
+import os
+
+
+# Defining main_menu
+def main_menu(self):
+    pass # TODO: Stub
