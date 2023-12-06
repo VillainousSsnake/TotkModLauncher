d3cc7eade2c2c50207fb9c89a295a56269549e70
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-12-06 22:16:27 +0000

    Added zs.py

---
+++ lib/zs.py
@@ -0,0 +1,17 @@
+# TotkModLauncher/lib/zs.py
+# Contains zs class
+
+# Importing modules
+import zstandard
+
+
+# Defining zs
+class zs:
+    
+    # Defining __init__
+    def __init__(self):
+        pass  # TODO: Stub
+
+    # Defining get_dict
+    def get_dict(self, mode):
+        pass # TODO: Stub
