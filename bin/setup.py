6c4aa98d633e5675492429834d53cac874f158af
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-12-06 18:58:38 +0000

    Added setup.py

---
+++ bin/setup.py
@@ -0,0 +1,23 @@
+"""
+INSTALLER VERSION: v1.0
+INSTALLS 0x1de-NX VERSION: v0.0.1
+"""
+# TotkModLauncher/bin/setup.py
+# Currently installs pip dependencies
+
+import subprocess
+import sys
+
+
+print("Installing dependencies")
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+for pk in ["SarcLib", "sarc", "zstandard", "rstb"]:
+    install(pk)
+
+
+print("Finished Installing Dependencies\nRun main.py to use the app!")
