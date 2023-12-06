d7cc71898c5d85bba8b5dd6b09bf4c388f12e033
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-12-06 20:27:44 +0000

    Added manage_menu.py

---
+++ gui/manage_menu.py
@@ -0,0 +1,11 @@
+# TotkModLauncher/gui/manage_menu.py
+# Contains manage_menu function
+
+# Importing libraries
+import tkinter
+import os
+
+
+# Defining manage_menu
+def manage_menu(self):
+    pass  # TODO: Stub
