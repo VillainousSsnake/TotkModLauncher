d5d474e23502d49ca460287ffed6a8158ee5c129
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-12-06 19:05:37 +0000

    Added settings_menu.py

---
+++ gui/settings_menu.py
@@ -0,0 +1,11 @@
+# TotkModLauncher/gui/settings_menu.py
+# Contains settings_menu function
+
+# Importing dependencies
+import tkinter
+import os
+
+
+# Defining settings_menu
+def settings_menu(self):
+    pass  # TODO: Stub
