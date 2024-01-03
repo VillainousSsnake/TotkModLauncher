4ff22e51c25c051f4332877dd207a893f927326a
VillainousSsnake <cjdiamond9@gmail.com>
2024-01-03 18:24:15 -0500

    Added and re-configured Buttons

--- gui/main_menu.py
+++ gui/main_menu.py
@@ -15,6 +15,7 @@ def main_menu(self):
     root.title("TotkModLauncher - Main Menu")
     root.geometry("500x500+200+200")
     root.configure(bg="#131642")
+    root.resizable(False, True)
 
     # Making the title bar dark mode
     root.update()
@@ -38,14 +39,38 @@ def main_menu(self):
         root.destroy()
         self.returnStatement = "MANAGE"
 
+    # Defining settings_button_command function
+    def settings_button_command():
+        root.destroy()
+        self.returnStatement = "MANAGE"
+
     # Creating, initializing, and displaying Buttons
     # Setting up the launchButton
-    launchButton = tk.Button(root, text="      Launch      ", command=launch_button_command)
-    launchButton.pack(side="bottom")
+    launchButton = tk.Button(root,
+                             text="Launch",
+                             command=launch_button_command,
+                             width=69,
+                             bg="#8b41bf", borderwidth=2,
+                             highlightcolor="#ba75eb")
+    launchButton.pack(side="top")
 
     # Setting up manageButton
-    manageButton = tk.Button(root, text="Manage Mods", command=manage_button_command)
-    manageButton.pack(side="bottom")
+    manageButton = tk.Button(root,
+                             text="Manage Mods",
+                             command=manage_button_command,
+                             width=69,
+                             bg="#8b41bf", borderwidth=2,
+                             highlightcolor="#ba75eb")
+    manageButton.pack(side="top")
+
+    # Setting up settingsButton
+    settingsButton = tk.Button(root,
+                               text="Settings",
+                               command=settings_button_command,
+                               width=69,
+                               bg="#8b41bf", borderwidth=2,
+                               highlightcolor="#ba75eb")
+    settingsButton.pack(side="top")
 
     # mainloop
     root.mainloop()
