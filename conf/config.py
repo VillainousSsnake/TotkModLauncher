35fc2355013e20af0f01cb2788381d5e9c3a9b79
VillainousSsnake <cjdiamond9@gmail.com>
2024-01-03 18:11:07 -0500

    added conf/.

---
+++ conf/config.py
@@ -0,0 +1,11 @@
+# TODO: Stub
+
+class Config:
+    """
+    Config Class - Holds functions to read/write App configurations
+    """
+
+    @staticmethod
+    def get_configuration(entry):
+        """get_configuration(entry) - returns the value of the inputted entry."""
+        pass  # TODO: Stub
